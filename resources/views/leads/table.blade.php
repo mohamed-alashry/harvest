c3715556fe47342d8e3bf78bdc91910f8d13d072
mohammed-ashry <mohammed.ashry@ad.net.sa>
2021-07-14 22:19:27 +0200

    hash lead payments

--- resources/views/leads/table.blade.php
+++ resources/views/leads/table.blade.php
@@ -7,7 +7,7 @@
                 <th>Mobile</th>
                 <th>Assigned Employee</th>
                 <th>Cases</th>
-                <th>Payments</th>
+                {{-- <th>Payments</th> --}}
                 <th>Action</th>
             </tr>
         </thead>
@@ -17,15 +17,15 @@
                     <td>{{ $lead->created_at }}</td>
                     <td>{{ $lead->name['en'] }}</td>
                     <td>{{ $lead->mobile_1 }}</td>
-                    <td>{{ $lead->assignedEmployee->name }}</td>
+                    <td>{{ $lead->assignedEmployee->name ?? '' }}</td>
                     <td>
                         <a href="{{ route('admin.leadCases.index', ['lead' => $lead->id]) }}"
                             class="btn btn-warning">{{ $lead->cases_count }}</a>
                     </td>
-                    <td>
+                    {{-- <td>
                         <a href="{{ route('admin.leadPayments.index', ['lead' => $lead->id]) }}"
                             class="btn btn-warning">{{ $lead->payments_count }}</a>
-                    </td>
+                    </td> --}}
                     <td>
                         {!! Form::open(['route' => ['admin.leads.destroy', $lead->id], 'method' => 'delete']) !!}
                         <div class='btn-group'>
